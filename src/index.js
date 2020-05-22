e1b20b3aec911993439487413beddc9df6f62bf7
Maxi Bartolozzi <mxbarto@gmail.com>
2020-05-22 08:59:18 -0300

    Ouput Management with Webpack

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+import printMe from './print.js';
+
 
 function component(){
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     
     element.innerHTML = _.join(['hello','webpack'], ' ');
-    element.classList.add('hello');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
+    element.appendChild(btn);
 
     return element;
 }
